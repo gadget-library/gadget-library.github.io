9b1c3a71197434726fb4de9446a80207ea936abb
yashketanagnihotri <78458048+yashketanagnihotri@users.noreply.github.com>
2021-08-10 17:21:07 +0530

    Update todo.js

--- 2/todo.js
+++ 2/todo.js
@@ -1,20 +1,27 @@
 /*the is a program for a todo list*/
+let i=0
 function add(a)
 {
     let data=prompt("Enter the desired data")
+    window.localStorage.setItem(i,data)
     a.push(data);
+    i+=1;
     alert('Your data has been added')
 }
 function del(a)
 {
     if(a.pop())
-        alert('Element deleted')
+        {
+            alert('Element deleted')
+            window.localStorage.removeItem(i);
+            i-=1;
+        }
     else
         alert('No Element to delete')
 }
 function see(a)
 {
-    alert(`The elements in the array are\n`+a.join('\n'))
+    alert(`The elements in the array are\n`+ window.localStorage.getItem(i-1))
 }
 alert('This program will be a todo list\nYou can choose any of the following options\n1.Add\n2.Delete\n3.View\n4.Exit')
 let opt
